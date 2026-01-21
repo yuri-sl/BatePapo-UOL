f82a2041747a89f3deecf4bbe6ea7041438ec7d0
Yuri Santana <yuri03ysl@gmail.com>
2026-01-20 22:47:26 -0300

    Adding files

---
+++ src/test/java/bate/papo/uol/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package bate.papo.uol;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
